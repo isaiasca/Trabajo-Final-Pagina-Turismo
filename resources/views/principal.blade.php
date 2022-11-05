7e7caba475e8b76dc58fc491730e6b1b207209d7
isaiasca <1307759@senati.pe>
2022-11-05 16:29:59 -0500

    diseño

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -10,13 +10,18 @@
 </head>
 <body>
     <style>
+        .body{
+            margin: 0;
+            background-attachment: fixed;
+        }
         .carousel-inner{
             width: 100%;
-            height: 400px;
+            height: 500px;
+            
         }
         .img-caru{
             width: 100%;
-            height: 400px;
+            height: 500px;
         }
     </style>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-interval="2000">
