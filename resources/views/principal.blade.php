8ba63a77d97fe91e889f7158fb2848950dba81e6
isaiasca <1307759@senati.pe>
2022-11-07 18:45:52 -0500

    sitios con estilos

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -167,46 +167,62 @@
         
         .img-sitios{
             width: 100%;
-            height: 250px;
+            height: 350px;
+            transition: 0.3s;
+            object-fit: cover;
+           
+        }
+        .conteiner-sitios-1:hover .img-sitios{
+            transform: scale(1.2);
         }
         .sitios{
-            margin-left: 200px;
-            margin-right: 200px;
+            margin-left: 245px;
+            margin-right: 245px;
             margin-top: 45px;
+            object-fit: cover;
         }
         .conteiner-sitios-1{
             display: inline-block;
-            width: 550px;
-            height: 350px;
-            border: 1px solid skyblue;
+            width: 500px;
+            height: 470px;
+            border: 1px solid #001D6E;
             background-color: transparent;
+            overflow: hidden;
+            transition: 0.3s;
+            object-fit: cover;
+            margin: 5px;
 
         }
         .conteiner-sitios-2{
             display: inline-block;
             margin-top: 15px;
-            width: 550px;
-            height: 350px;
-            border: 1px solid skyblue;
+            width: 500px;
+            height: 470px;
+            border: 1px solid #001D6E;
             background-color: transparent;
+            margin: 5px;
 
         }
         .conteiner-sitios-3{
             display: inline-block;
             margin-top: 15px;
-            width: 550px;
-            height: 350px;
-            border: 1px solid skyblue;
+            width: 500px;
+            height: 470px;
+            border: 1px solid #001D6E;
             background-color: transparent;
+            margin: 5px;
 
         }
         .titulo-hua{
-            font-size: 25px;
-            
+            font-size: 33px;
+            color: #069A8E;
+            font-family: "Garamod", cursive;
         }
         .p-hua{
-            font-size: 15px;
+            font-size: 19px;
             text-align: center;
+            margin-top: 14px;
+            font-family: "didot";
         }
         
     </style>
@@ -217,33 +233,33 @@
     <div class="sitios">
         <div class="conteiner-sitios-1">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Font_del_Parc_de_San_Sebasti%C3%A1n_amb_l%27esgl%C3%A9sia_de_San_Sebasti%C3%A1n_darrera_a_Hu%C3%A1nuco.jpg/1200px-Font_del_Parc_de_San_Sebasti%C3%A1n_amb_l%27esgl%C3%A9sia_de_San_Sebasti%C3%A1n_darrera_a_Hu%C3%A1nuco.jpg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua">HUÁNUCO</a>
-            <p class="p-hua"> como estas hoy</p>
+            <a href="" class="titulo-hua"><center>HUÁNUCO</center></a>
+            <p class="p-hua">Ciudad con el mejor clima del mundo</p>
         </div>
         <div class="conteiner-sitios-1">
             <img src="https://www.turiweb.pe/wp-content/uploads/2020/07/tumbes-160720.jpg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua">TUMBES</a>
-            <p class="p-hua">como estas en este dia</p>
+            <a href="" class="titulo-hua"><center>TUMBES</center></a>
+            <p class="p-hua">Entre manglares y ballenas</p>
         </div>
         <div class="conteiner-sitios-2">
             <img src="https://www.magellanmag.com/blog/wp-content/uploads/2020/08/Trujillo2.jpg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua">TRUJILLO</a>
-            <p class="p-hua"> como estas hoy</p>
+            <a href="" class="titulo-hua"><center>TRUJILLO</center></a>
+            <p class="p-hua">Bienvenidos a la ciudad blanca</p>
         </div>
         <div class="conteiner-sitios-2">
             <img src="https://elcomercio.pe/resizer/rJ_stzLvYw-NWlJkC8-pxijRX-M=/1200x900/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/EZHTSW4PEFHUBES2XS7AT6X4KM.jpg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua">TINGO MARÍA</a>
-            <p class="p-hua">como estas en este dia</p>
+            <a href="" class="titulo-hua"><center>TINGO MARÍA</center></a>
+            <p class="p-hua">Ciudad de la bella durmiente</p>
         </div>
         <div class="conteiner-sitios-3">
             <img src="https://media.viajando.travel/p/6c685a37a1c8611b51532b6d1ee3b53c/adjuntos/277/imagenes/000/423/0000423605/1200x675/smart/cuscojpeg.jpeg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua"> <center> CUSCO </center></a>
-            <p class="p-hua"> como estas hoy</p>
+            <a href="" class="titulo-hua"><center>CUSCO</center></a>
+            <p class="p-hua">Imperio viviente</p>
         </div>
         <div class="conteiner-sitios-3">
             <img src="https://1.bp.blogspot.com/-2Ffhhra459M/YSvG3Cl1lSI/AAAAAAABwJs/SYaXc5FfFOMhjY1V2lgy_PEkQ60d5Gp7ACLcBGAsYHQ/s640/nasca-plaza.jpg" class="img-sitios" alt="">
-            <a href="" class="titulo-hua">NAZCA</a>
-            <p class="p-hua">como estas en este dia</p>
+            <a href="" class="titulo-hua"><center>NAZCA</center></a>
+            <p class="p-hua">Bienvenidos a los gigantescos geoglifos</p>
         </div>
     </div>
 </body>
