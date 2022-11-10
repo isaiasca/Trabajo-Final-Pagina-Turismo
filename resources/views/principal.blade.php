9ec34707033c71585ab57456b540202b27e203aa
isaiasca <1307759@senati.pe>
2022-11-09 19:27:47 -0500

    encabezado

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -9,6 +9,69 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
+    <!--iniciodel encabezado-->
+    <style>
+        .img-encabezado{
+            width: 200px;
+            margin-left: 100px;
+        }
+        .lista{
+            float: left;
+            list-style: none;
+            padding: 10px;
+        }
+        .conteiner-lista-com{
+            float: right;
+            margin-top: 0px;
+            margin-right: 200px;
+            
+        }
+        .link-lista{
+            text-decoration: none;
+            font-family: garamot;
+            font-weight: 600;
+            color: #3C4048;
+            cursor: pointer;
+            margin: 0 1rem;
+        }
+        .lista:hover .link-lista{
+            color: #3A8891;
+        }
+        #header{
+            height: auto;
+            width: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: space-between;
+            padding: 0 5%;
+            box-sizing: border-box;
+            height: 70px;
+            background-color: beige;
+            transition: 0.5s;
+        }
+        .main-menu{
+            
+            justify-content: center;
+        }
+        
+    </style>
+    <header id="header">
+        <img src="https://www.estudionovaidea.com/images/travel-zone.png" class="img-encabezado" alt="">
+        <nav class="conteiner-lista-com">
+            <ul class="main-menu">
+                <li class="lista"><a href="" class="link-lista">Home</a></li>
+                <li class="lista"><a href="" class="link-lista">Quienes somos</a></li>
+                <li class="lista"><a href="" class="link-lista">Oferta</a></li>
+                <li class="lista"><a href="" class="link-lista">Tours</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!--fin del encabezado--> 
+    
+    <!--comenzamos carusel-->
     <style>
         
         .body{
@@ -370,5 +433,6 @@
             </a>
         </div>
     </div>
+    <!--fin sitios turisticos-->
 </body>
 </html>
--- resources/views/probar.blade.php
+++ resources/views/probar.blade.php
@@ -109,9 +109,67 @@
         
             
     </style>
-    <h1>hola</h1>
-
+    <!--iniciodel encabezado-->
+    <style>
+        .img-encabezado{
+            width: 200px;
+            margin-left: 100px;
+        }
+        .lista{
+            float: left;
+            list-style: none;
+            padding: 10px;
+        }
+        .conteiner-lista-com{
+            float: right;
+            margin-top: 0px;
+            margin-right: 200px;
             
+        }
+        .link-lista{
+            text-decoration: none;
+            font-family: garamot;
+            font-weight: 600;
+            color: #3C4048;
+            cursor: pointer;
+            margin: 0 1rem;
+        }
+        .lista:hover .link-lista{
+            color: #3A8891;
+        }
+        #header{
+            height: auto;
+            width: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: space-between;
+            padding: 0 5%;
+            box-sizing: border-box;
+            height: 70px;
+            background-color: beige;
+            transition: 0.5s;
+        }
+        .main-menu{
+            display: flex;
+            justify-content: center;
+        }
+        
+    </style>
+    <header id="header">
+        <img src="https://www.estudionovaidea.com/images/travel-zone.png" class="img-encabezado" alt="">
+        <nav class="conteiner-lista-com">
+            <ul class="main-menu">
+                <li class="lista"><a href="" class="link-lista">Home</a></li>
+                <li class="lista"><a href="" class="link-lista">Quienes somos</a></li>
+                <li class="lista"><a href="" class="link-lista">Oferta</a></li>
+                <li class="lista"><a href="" class="link-lista">Tours</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!--fin del encabezado-->        
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-interval="2000">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
